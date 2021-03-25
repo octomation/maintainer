1e6ea4cb64938b2a7c9c1b6704bc290411700ebf
Kamil Samigullin <kamil@samigullin.info>
2021-03-25 13:42:48 +0300

    issue #13: isolate github commands

--- internal/cmd/github/labels.go
+++ internal/cmd/github/labels.go
@@ -28,114 +28,133 @@ func Labels(git Git, github GitHub) *cobra.Command {
 		Long:  "Manage repository labels.",
 	}
 
-	patch := cobra.Command{
-		Args:  cobra.ExactArgs(1),
-		Use:   "patch",
-		Short: "patch repository labels",
-		Long:  "Patch repository labels.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var current model.LabelSet
+	{
+		patch := cobra.Command{
+			Args:  cobra.ExactArgs(1),
+			Use:   "patch",
+			Short: "patch repository labels",
+			Long:  "Patch repository labels.",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				var current model.LabelSet
 
-			in := cmd.InOrStdin()
-			if input != "" {
-				f, err := os.Open(input)
+				in := cmd.InOrStdin()
+				if input != "" {
+					f, err := os.Open(input)
+					if err != nil {
+						return err
+					}
+					defer safe.Close(f, unsafe.Ignore)
+					in = f
+				}
+
+				if err := yaml.NewDecoder(in).Decode(&current); err != nil {
+					return err
+				}
+
+				out := cmd.OutOrStdout()
+				if output != "" {
+					f, err := os.Open(output)
+					if err != nil {
+						return err
+					}
+					defer safe.Close(f, unsafe.Ignore)
+					out = f
+				}
+
+				assert.True(func() bool { return len(args) == 1 })
+				patched, err := github.PatchLabels(cmd.Context(), current, args[0])
 				if err != nil {
 					return err
 				}
-				defer safe.Close(f, unsafe.Ignore)
-				in = f
-			}
 
-			if err := yaml.NewDecoder(in).Decode(&current); err != nil {
-				return err
-			}
+				sort.Sort(model.SortLabelsByName(patched))
+				return yaml.NewEncoder(out).Encode(patched)
+			},
+		}
+		flags := patch.Flags()
+		flags.StringVar(&input, "input", "", "input file with labels [stdin]")
+		flags.StringVar(&output, "output", "", "output file to store labels [stdout]")
+		command.AddCommand(&patch)
+	}
 
-			out := cmd.OutOrStdout()
-			if output != "" {
-				f, err := os.Open(output)
+	{
+		pull := cobra.Command{
+			Args:  cobra.NoArgs,
+			Use:   "pull",
+			Short: "pull repository labels",
+			Long:  "Pull repository labels.",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				remotes, err := git.Remotes()
+				if err != nil {
+					return fmt.Errorf("cannot specify repository: %w", err)
+				}
+
+				remote, found := remotes.GitHub()
+				if !found {
+					return fmt.Errorf("cannot find GitHub repository")
+				}
+				src := model.Remote(remote)
+
+				labels, err := github.Labels(cmd.Context(), src)
+				if err != nil {
+					return fmt.Errorf("cannot fetch repository labels: %w", err)
+				}
+
+				sort.Sort(model.SortLabelsByName(labels))
+				return yaml.NewEncoder(cmd.OutOrStdout()).Encode(labels)
+			},
+		}
+		command.AddCommand(&pull)
+	}
+
+	{
+		push := cobra.Command{
+			Args:  cobra.NoArgs,
+			Use:   "push",
+			Short: "push repository labels",
+			Long:  "Push repository labels.",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				remotes, err := git.Remotes()
+				if err != nil {
+					return fmt.Errorf("cannot specify repository: %w", err)
+				}
+
+				remote, found := remotes.GitHub()
+				if !found {
+					return fmt.Errorf("cannot find GitHub repository")
+				}
+
+				_ = remote
+				return nil
+			},
+		}
+		command.AddCommand(&push)
+	}
+
+	{
+		sync := cobra.Command{
+			Args:  cobra.NoArgs,
+			Use:   "sync",
+			Short: "sync repository labels",
+			Long:  "Sync repository labels.",
+			RunE: func(cmd *cobra.Command, args []string) error {
+				push, args, err := cmd.Parent().Find([]string{"push"})
 				if err != nil {
 					return err
 				}
-				defer safe.Close(f, unsafe.Ignore)
-				out = f
-			}
+				if err := push.RunE(push, args); err != nil {
+					return err
+				}
 
-			assert.True(func() bool { return len(args) == 1 })
-			patched, err := github.PatchLabels(cmd.Context(), current, args[0])
-			if err != nil {
-				return err
-			}
-
-			sort.Sort(model.SortLabelsByName(patched))
-			return yaml.NewEncoder(out).Encode(patched)
-		},
+				pull, args, err := cmd.Parent().Find([]string{"pull"})
+				if err != nil {
+					return err
+				}
+				return pull.RunE(cmd, args)
+			},
+		}
+		command.AddCommand(&sync)
 	}
-	flags := patch.Flags()
-	flags.StringVar(&input, "input", "", "input file with labels [stdin]")
-	flags.StringVar(&output, "output", "", "output file to store labels [stdout]")
-
-	pull := cobra.Command{
-		Args:  cobra.NoArgs,
-		Use:   "pull",
-		Short: "pull repository labels",
-		Long:  "Pull repository labels.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			remotes, err := git.Remotes()
-			if err != nil {
-				return fmt.Errorf("cannot specify repository: %w", err)
-			}
-
-			remote, found := remotes.GitHub()
-			if !found {
-				return fmt.Errorf("cannot find GitHub repository")
-			}
-			src := model.Remote(remote)
-
-			labels, err := github.Labels(cmd.Context(), src)
-			if err != nil {
-				return fmt.Errorf("cannot fetch repository labels: %w", err)
-			}
-
-			sort.Sort(model.SortLabelsByName(labels))
-			return yaml.NewEncoder(cmd.OutOrStdout()).Encode(labels)
-		},
-	}
-
-	push := cobra.Command{
-		Args:  cobra.NoArgs,
-		Use:   "push",
-		Short: "push repository labels",
-		Long:  "Push repository labels.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			remotes, err := git.Remotes()
-			if err != nil {
-				return fmt.Errorf("cannot specify repository: %w", err)
-			}
-
-			remote, found := remotes.GitHub()
-			if !found {
-				return fmt.Errorf("cannot find GitHub repository")
-			}
-
-			_ = remote
-			return nil
-		},
-	}
-
-	sync := cobra.Command{
-		Args:  cobra.NoArgs,
-		Use:   "sync",
-		Short: "sync repository labels",
-		Long:  "Sync repository labels.",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := push.RunE(cmd, args); err != nil {
-				return err
-			}
-			return pull.RunE(cmd, args)
-		},
-	}
-
-	command.AddCommand(&pull, &patch, &push, &sync)
 
 	return &command
 }
