ec6e8328dc33548a2e7423495c92bf7b7e3e68ed
Kamil Samigullin <kamil@samigullin.info>
2023-02-24 08:16:36 +0300

    tools(deps): update dependency list and versions

--- tools/tools.go
+++ tools/tools.go
@@ -6,18 +6,20 @@ package tools
 
 import (
 	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+//	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/marwan-at-work/mod/cmd/mod"
 	_ "golang.org/x/exp/cmd/gorelease"
 	_ "golang.org/x/tools/cmd/benchcmp"
+	_ "golang.org/x/tools/cmd/goimports"
 	_ "golang.org/x/tools/cmd/gomvpkg"
 	_ "golang.org/x/tools/cmd/gorename"
 )
 
 //go:generate go install github.com/golang/mock/mockgen
-//go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
+// go:generate go install github.com/golangci/golangci-lint/cmd/golangci-lint
 //go:generate go install github.com/marwan-at-work/mod/cmd/mod
 //go:generate go install golang.org/x/exp/cmd/gorelease
 //go:generate go install golang.org/x/tools/cmd/benchcmp
+//go:generate go install golang.org/x/tools/cmd/goimports
 //go:generate go install golang.org/x/tools/cmd/gomvpkg
 //go:generate go install golang.org/x/tools/cmd/gorename
