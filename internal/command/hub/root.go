3e8613e5d1e66cda13204584c74ad48a9310fc63
Kamil Samigullin <kamil@samigullin.info>
2023-04-22 08:45:16 +0300

    feat(deps): up github to v86

--- internal/command/hub/root.go
+++ internal/command/hub/root.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"sync"
 
-	"github.com/google/go-github/v85/github"
+	"github.com/google/go-github/v86/github"
 	"github.com/spf13/cobra"
 	"go.octolab.org/async"
 	"go.octolab.org/safe"
--- internal/service/github/service.go
+++ internal/service/github/service.go
@@ -3,7 +3,7 @@ package github
 import (
 	"net/http"
 
-	"github.com/google/go-github/v85/github"
+	"github.com/google/go-github/v86/github"
 )
 
 // New returns a new GitHub service.
