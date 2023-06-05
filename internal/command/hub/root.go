4db1d18aac97a293d48172b4142f4933914075a5
Kamil Samigullin <kamil@samigullin.info>
2023-06-05 08:27:25 +0300

    feat(deps): up github.com/google/go-github to v53

--- internal/command/hub/root.go
+++ internal/command/hub/root.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"sync"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"github.com/spf13/cobra"
 	"go.octolab.org/async"
 	"go.octolab.org/safe"
